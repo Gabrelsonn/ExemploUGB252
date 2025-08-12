113c83323e7f2f0c136caac5170d06d8b5f881b5
Gabrelsonn <gabrielseixas117@gmail.com>
2025-08-12 19:50:35 -0300

    Acrescentei o else

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -2,6 +2,7 @@
 int idade = 24;
 Console.WriteLine("A idade é" + idade);
 if (idade >= 18)
-{
     Console.WriteLine("Você pode tirar a habilitação");
-}
+
+else
+    Console.WriteLine("Você não pode tirar a habilitação");
