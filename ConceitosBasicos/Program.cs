fc9032709c7e255625734d807f67da30c541bf22
Gabrelsonn <gabrielseixas117@gmail.com>
2025-08-12 20:41:35 -0300

    Exercicio

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -3,3 +3,5 @@ string nome = Console.ReadLine();
 Console.WriteLine("Informe seu salario: ");
 decimal salarioStr  = decimal.Parse(Console.ReadLine());
 Console.WriteLine($"Bem vindo {nome}, Você tem ${salarioStr} kwanzas.");
+
+//Informem o valor do IRPF a ser pago de acordo com salario informado. utilize a tabela real de desconto do IRPF
