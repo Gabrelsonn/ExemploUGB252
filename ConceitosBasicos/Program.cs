2e493aac73e0f98717b3da99fa8e297a853b2424
Gabrelsonn <gabrielseixas117@gmail.com>
2025-08-12 21:13:41 -0300

    Tentando

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -6,12 +6,14 @@ Console.WriteLine($"Bem vindo {nome}, Você tem ${salario} kwanzas.");
 
 //Informem o valor do IRPF a ser pago de acordo com salario informado. utilize a tabela real de desconto do IRPF
 
+decimal calculo = salario * 0.0075m
 
-
-if (salario >= 2259.20m) {
+if (salario >= 2259.20m)
+{
     Console.WriteLine("Isento");
 }
-else if (salario <= 2259.20m && salario >= 2826.65m) {
+else if (salario <= 2259.20m && salario >= 2826.65m)
+{
     Console.WriteLine("Terá que pagar a aliquota de 7,5%");
 }
 else if (salario <= 2826.65m && salario >= 3751.05m)
@@ -23,8 +25,7 @@ else if (salario <= 2751.05m && salario >= 4664.68m)
     Console.WriteLine("Terá que pagar a aliquota de 22,5%-R$662,77");
 }
 else
-    salario <= 4664.68m
-        Console.WriteLine("Terá que pagar a aliquota de 27,5%-R$896,00");
+    Console.WriteLine("Terá que pagar a aliquota de 27,5%-R$896,00");
 
 
 
