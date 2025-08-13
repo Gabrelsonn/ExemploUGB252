75d350ca732d8caad5688c72711606e90ac3c9e0
Gabrelsonn <gabrielseixas117@gmail.com>
2025-08-13 11:11:44 -0300

    Perfeito

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -6,26 +6,22 @@ Console.WriteLine($"Bem vindo {nome}, Você tem ${salario} kwanzas.");
 
 //Informem o valor do IRPF a ser pago de acordo com salario informado. utilize a tabela real de desconto do IRPF
 
-decimal calculo = salario * 0.0075m
+var calculo = salario * 0.075m;
+var calculo1 = salario * 0.15m;
+var calculo2 = salario * 0.225m;
+var calculo3 = salario * 0.275m;
 
-if (salario >= 2259.20m)
-{
+if (salario <= 2259.20m)
     Console.WriteLine("Isento");
-}
-else if (salario <= 2259.20m && salario >= 2826.65m)
-{
-    Console.WriteLine("Terá que pagar a aliquota de 7,5%");
-}
-else if (salario <= 2826.65m && salario >= 3751.05m)
-{
-    Console.WriteLine("Terá que pagar a aliquota de 15%-R$381,44");
-}
-else if (salario <= 2751.05m && salario >= 4664.68m)
-{
-    Console.WriteLine("Terá que pagar a aliquota de 22,5%-R$662,77");
-}
+else if (salario >= 2259.20m && salario <= 2826.65m)
+    Console.WriteLine($"Terá que pagar a aliquota de 7,5% que é {calculo}");
+else if (salario >= 2826.65m && salario <= 3751.05m)
+    Console.WriteLine($"Terá que pagar a aliquota de 15% que é {calculo1}");
+else if (salario >= 2751.05m && salario <= 4664.68m)
+    Console.WriteLine($"Terá que pagar a aliquota de 22,5% que é {calculo2}");
 else
-    Console.WriteLine("Terá que pagar a aliquota de 27,5%-R$896,00");
+    Console.WriteLine($"Terá que pagar a aliquota de 27,5% que é {calculo3}");
+
 
 
 
